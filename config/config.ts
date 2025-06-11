2969150384186d573ddd4b934673aaf530a5278f
py0l <535140532@qq.com>
2025-06-11 15:20:45 +0800

    CI: 更新部署工作流权限并启用hash路由
    -更新GitHub Actions部署工作流权限，从`pages: write`和`packages: write`到`contents: write`，以获得更广泛的访问权限。
    -在config. js中启用哈希历史记录。确保生产环境中正确的路由。

--- config/config.ts
+++ config/config.ts
@@ -8,9 +8,9 @@ const { REACT_APP_ENV = 'dev' } = process.env;
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/xadmin/' : '/',
   base: process.env.NODE_ENV === 'production' ? '/xadmin/' : '/',
-  // history: {
-  //   type: 'hash',
-  // },
+  history: {
+    type: 'hash',
+  },
   /**
    * @name 开启 hash 模式
    * @description 让 build 之后的产物包含 hash 后缀。通常用于增量发布和避免浏览器加载缓存。
