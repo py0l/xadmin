b5c2c453383bed9778d77ac30ed2c83b9b76552f
py0l <535140532@qq.com>
2025-06-17 16:40:42 +0800

    feat: 为test-mock环境配置publicPath
    当`REACT_APP_ENV`为`test-mock`时，将`publicPath`设置为`/xadmin/`，以确保在该环境中正确加载资源。否则，`publicPath`仍然是`/`。

--- config/config.ts
+++ config/config.ts
@@ -6,6 +6,7 @@ import proxy from './proxy';
 import routes from './routes';
 const { REACT_APP_ENV = 'dev' } = process.env;
 export default defineConfig({
+  publicPath: REACT_APP_ENV === 'test-mock' ? '/xadmin/' : '/',
   define: {
     'process.env.REACT_APP_ENV': REACT_APP_ENV,
   },
